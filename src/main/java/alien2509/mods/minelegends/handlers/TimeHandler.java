ee1dd92f173cc2bb22ccd25d866162fa2a16ee22
alien2509 <alien2509@hotmail.com>
2015-06-03 20:16:02 +0200

    Made a start on a date and time handler

--- src/main/java/alien2509/mods/minelegends/handlers/TimeHandler.java
+++ src/main/java/alien2509/mods/minelegends/handlers/TimeHandler.java
@@ -1,9 +1,24 @@
 package alien2509.mods.minelegends.handlers;
 
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.ServerTickEvent;
+import alien2509.mods.minelegends.util.Date;
 
 public class TimeHandler
 {
+	
+	public static Map<String, Date> Dates = new HashMap<String, Date>();
+	
+	public static void registerDate(Date date)
+	{
+		
+		Dates.put(date.getName(), date);
+		
+	}
 	
 	public static Date getDateFromTicks(long ticks)
 	{
@@ -16,6 +31,20 @@ public class TimeHandler
 	{
 		
 		return null;
+		
+	}
+	
+	@SubscribeEvent
+	public static void onTickUpdate(ServerTickEvent event)
+	{
+		
+		for(Entry<String, Date> entry : Dates.entrySet())
+		{
+			
+			Date currentDate = (Date)entry;
+			currentDate.onTick();
+			
+		}
 		
 	}
 	
