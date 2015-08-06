196e9a0710d33bea7eebdc24e9c9446364caf253
alien2509 <bart.vanden.dool@DT-JoJo>
2015-08-06 17:36:43 +0200

    added the server tick event for the pregenerators

--- src/main/java/alien2509/mods/minelegends/handlers/GenerationHandler.java
+++ src/main/java/alien2509/mods/minelegends/handlers/GenerationHandler.java
@@ -1,23 +1,59 @@
 package alien2509.mods.minelegends.handlers;
 
+import java.util.List;
+
+import alien2509.mods.minelegends.world.gen.PreGenerator;
+import net.minecraft.world.ChunkCoordIntPair;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.ServerTickEvent;
 
 public class GenerationHandler
 {
 	
+	public static List<PreGenerator> pregenerators;
+	
+	static double multiplier = 1.0d;
+	static double overflow = .0d;
+	
 	@SubscribeEvent
 	public static void onServerTick(ServerTickEvent event)
 	{
 		
+		PreGenerator toRun = pregenerators.get(0);
 		
+		double target = multiplier + overflow;
+		int liTarget = (int) Math.floor(target);
+		overflow =  target - liTarget;
+		
+		long delta = System.currentTimeMillis();
+		for(int i = 0; i < liTarget; i++)
+		{
+			toRun.generateChunk();
+		}
+		delta = System.currentTimeMillis() - delta;
+		
+		if(delta > 50.0d)
+		{
+			multiplier -= .1d;
+		} else
+		{
+			multiplier += .1d;
+		}
+		
+		if(multiplier > 100.0d) multiplier = 100.0d;
+		if(multiplier < 1.0d) multiplier = 1.0d;
+		
+		if(toRun.done == true)
+		{
+			pregenerators.remove(0);
+		}
 		
 	}
 	
-	public static void preGenerateWorlds(int[] worlds)
+	public static void createWorldPreGenerator(ChunkCoordIntPair center, int range, int DimID)
 	{
 		
-		
+		pregenerators.add(new PreGenerator(center, range, DimID));
 		
 	}
 	
